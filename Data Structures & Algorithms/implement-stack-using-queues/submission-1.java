d0bbe2d813e8bba54066af1896ef5ded0fc2c957
Maksym Dmyterko <maksym.dmyterko@uzh.ch>
2026-05-24 22:37:24 +0200

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.java
@@ -0,0 +1,45 @@
+class MyStack {
+    private Queue<Integer> queue; // lifo
+
+    public MyStack() {
+        this.queue = new LinkedList();
+    }
+
+    public void push(int x) {
+        this.queue.offer(x);
+    }
+
+    public int pop() { // gets the front of queue
+        for (int i = 0; i < this.queue.size() - 1; i++) {
+            queue.offer(queue.poll());
+        }
+        return queue.poll();
+    }
+
+    public int top() { // gets the front of queue
+        for (int i = 0; i < this.queue.size() - 1; i++) {
+            queue.offer(queue.poll());
+        }
+        int res = this.queue.poll();
+        this.queue.offer(res); // to restore the order for future pop() operations
+        return res;
+    }
+
+    public boolean empty() {
+        return this.queue.size() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+
+    Throws exception	Returns special value
+Insert	add(e)	        offer(e)
+Remove	remove()	    poll()
+Examine	element()	    peek()
+ */
