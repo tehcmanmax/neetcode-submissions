07a2afa31ae6442061b608688544baa1568848c9
Maksym Dmyterko <maksym.dmyterko@uzh.ch>
2026-06-19 16:54:54 +0200

    Add: python-stack-push-pop - submission-1

---
+++ Python Coding Interviews/python-stack-push-pop/submission-1.py
@@ -0,0 +1,18 @@
+# Challenge
+# Implement the following function using the stack operations described above:
+# reverse_list(arr: List[int]) -> List[int] that takes a list of integers and 
+# returns a new list of the integers in reverse order.
+# Hint: Recall that elements from a stack are removed in reverse order.
+
+
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    return arr[::-1] # list coprehension
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
