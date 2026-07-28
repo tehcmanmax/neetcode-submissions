f4d67f2e2d845dd9a921026f6815b87cd6903af6
Maksym Dmyterko <maksym.dmyterko@uzh.ch>
2026-07-28 14:56:35 +0200

    Add: python-max-heap - submission-1

---
+++ Python Coding Interviews/python-max-heap/submission-1.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    neg_heap = []
+    output = []
+
+    for v in nums:
+        heapq.heappush(neg_heap, -v)
+    while neg_heap:
+        output.append(-heapq.heappop(neg_heap))
+    return output
+    
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
+
+#Implement the function get_reverse_sorted(nums: List[int]) -> List[int]
+# which takes a list of integers and returns the integers in reverse sorted order. 
+#You should use the max heap technique described above to achieve this. The list of integers given is not necessarily a heap.
+
